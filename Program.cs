902d499105ce40ea3379a7f54223c60bf1901d27
Isac Lagerström <isac.lagerstrom@gmail.com>
2021-11-09 11:35:51 +0100

    Added OpenGL and made a triangle

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GLFW;
+using static OpenGL.Gl;
 
 namespace SharpEngine
 {
@@ -7,16 +8,44 @@ namespace SharpEngine
     {
         static void Main(string[] args)
         {
-            Glfw.Init();
+            
+            
+                Glfw.Init();
+               
 
-            var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
-            Glfw.MakeContextCurrent(window);
+                var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
+                Glfw.MakeContextCurrent(window);
+                Import(Glfw.GetProcAddress);
 
-            while (!Glfw.WindowShouldClose(window))
-            {
-                Glfw.PollEvents();
-            }
+                float[] verticies = new float[]
+                {
+                    -.5f, -.5f, 0f,
+                    .5f, -.5f, 0f,
+                     0f, .5f, 0f
 
+                };
+
+                var vertexArray = glGenVertexArray();
+                var vertexBuffer = glGenBuffer();
+                glBindVertexArray(vertexArray);
+                glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+                unsafe
+                {
+                    fixed (float* vertex = &verticies[0])
+                    {
+                        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * verticies.Length, vertex, GL_STATIC_DRAW);
+                    } 
+                    glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+                }
+                glEnableVertexAttribArray(0);
+                
+                while (!Glfw.WindowShouldClose(window))
+                {
+                    Glfw.PollEvents();
+                    glDrawArrays(GL_TRIANGLES,0,3);
+                    Glfw.SwapBuffers(window);
+                }
+            
         }
     }
 }
